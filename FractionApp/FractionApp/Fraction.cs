55c36048401dab49a42247d891abd4532791509e
ALex49495 <sashazinuk1@gmail.com>
2024-12-17 23:15:11 +0200

    Add files via upload

---
+++ FractionApp/FractionApp/Fraction.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace FractionApp
+{
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+        public Fraction()
+        {
+            Numerator = 0;
+            Denominator = 1;
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public Fraction(Fraction other)
+        {
+            Numerator = other.Numerator;
+            Denominator = other.Denominator;
+        }
+
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.Numerator, a.Denominator);
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Denominator + b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Denominator - b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Denominator, a.Denominator * b.Numerator);
+        }
+
+        public static bool operator >(Fraction a, Fraction b) => (double)a > (double)b;
+        public static bool operator >=(Fraction a, Fraction b) => (double)a >= (double)b;
+        public static bool operator <=(Fraction a, Fraction b) => (double)a <= (double)b;
+        public static bool operator <(Fraction a, Fraction b) => (double)a < (double)b;
+        public static bool operator ==(Fraction a, Fraction b) => (double)a == (double)b;
+        public static bool operator !=(Fraction a, Fraction b) => (double)a != (double)b;
+
+        public static explicit operator double(Fraction a)
+        {
+            return (double)a.Numerator / a.Denominator;
+        }
+
+        public void Reduce()
+        {
+            int gcd = GCD(Numerator, Denominator);
+            Numerator /= gcd;
+            Denominator /= gcd;
+        }
+
+        private int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Numerator: {Numerator}, Denominator: {Denominator}");
+        }
+    }
+}
---
+++ FractionApp/FractionApp/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FractionApp
+{
+    class Program
+    {
+        static List<Fraction> fractions = new List<Fraction>();
+
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("uk-UA");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("Виберіть завдання:");
+                Console.WriteLine("1. Створити об'єкти класу Fraction");
+                Console.WriteLine("2. Показати інформацію про об'єкт");
+                Console.WriteLine("3. Виконати арифметичні операції");
+                Console.WriteLine("4. Виконати порівняння об'єктів");
+                Console.WriteLine("5. Вийти");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateFractions();
+                        break;
+                    case "2":
+                        ShowInfo();
+                        break;
+                    case "3":
+                        PerformArithmeticOperations();
+                        break;
+                    case "4":
+                        CompareFractions();
+                        break;
+                    case "5":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Невірний вибір, спробуйте ще раз.");
+                        break;
+                }
+            }
+        }
+
+        static void CreateFractions()
+        {
+            fractions.Add(new Fraction());
+            fractions.Add(new Fraction(3, 4));
+            fractions.Add(new Fraction(fractions[1]));
+
+            Console.WriteLine("Фракції створено.");
+        }
+
+        static void ShowInfo()
+        {
+            foreach (var fraction in fractions)
+            {
+                fraction.ShowInfo();
+            }
+        }
+
+        static void PerformArithmeticOperations()
+        {
+            if (fractions.Count >= 2)
+            {
+                var result1 = fractions[0] + fractions[1];
+                var result2 = fractions[0] - fractions[1];
+                var result3 = fractions[0] * fractions[1];
+                var result4 = fractions[0] / fractions[1];
+
+                Console.WriteLine($"Додавання: {result1}");
+                Console.WriteLine($"Віднімання: {result2}");
+                Console.WriteLine($"Множення: {result3}");
+                Console.WriteLine($"Ділення: {result4}");
+            }
+            else
+            {
+                Console.WriteLine("Не достатньо фракцій для виконання операцій.");
+            }
+        }
+
+        static void CompareFractions()
+        {
+            if (fractions.Count >= 2)
+            {
+                var result1 = fractions[0] > fractions[1];
+                var result2 = fractions[0] >= fractions[1];
+                var result3 = fractions[0] < fractions[1];
+                var result4 = fractions[0] <= fractions[1];
+                var result5 = fractions[0] == fractions[1];
+                var result6 = fractions[0] != fractions[1];
+
+                Console.WriteLine($"fractions[0] > fractions[1]: {result1}");
+                Console.WriteLine($"fractions[0] >= fractions[1]: {result2}");
+                Console.WriteLine($"fractions[0] < fractions[1]: {result3}");
+                Console.WriteLine($"fractions[0] <= fractions[1]: {result4}");
+                Console.WriteLine($"fractions[0] == fractions[1]: {result5}");
+                Console.WriteLine($"fractions[0] != fractions[1]: {result6}");
+            }
+            else
+            {
+                Console.WriteLine("Не достатньо фракцій для порівняння.");
+            }
+        }
+    }
+}
